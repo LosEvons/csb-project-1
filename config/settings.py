0b61e50847fa8867de68a66fb00d81429ae9f900
LosEvons <emil.malk@gmail.com>
2026-08-29 12:31:34 +0300

    Updated fixes in settings.py

--- config/settings.py
+++ config/settings.py
@@ -10,11 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# FIX FOR SECRET: load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
@@ -22,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-1vxvy0f29-t53q(&8z713*(se&c-*p&em@ose6ng7%r$piiu1*" # FLAW! Commited secrets! Security misconfiguration!
 # FIX:
-# import os
-# SECRET_KEY = os.getenv("SECRET_KEY")
+# SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True # FLAW! Security misconfiguration!
@@ -89,7 +91,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [ # FLAW! Weak password policy!
+AUTH_PASSWORD_VALIDATORS = [ # FLAW! Weak password policy! FIX: Uncomment policies bellow.
 #    {
 #        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
 #    },
