3b9cadd6d8342e3361ceb5d39a1567ecb8670145
LosEvons <emil.malk@gmail.com>
2026-09-19 18:22:00 +0300

    Small formatting fix

--- config/settings.py
+++ config/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-1vxvy0f29-t53q(&8z713*(se&c-*p&em@ose6ng7%r$piiu1*" # FLAW! Commited secrets! Security misconfiguration!
-# FIX:
-# SECRET_KEY = os.environ["SECRET_KEY"]
+# FIX: SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True # FLAW! Security misconfiguration!
